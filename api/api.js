20ba89aa44db8cbb396da4ec1b8a92ed04af8c6f
Guo Yiyang <hua-honkai@qq.com>
2023-11-12 19:43:59 +0800

    Update api.js

--- api/api.js
+++ api/api.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
     let target = 'https://anycast.cloudflare.tunnel.cloud-private.eu.org'
-    if (req.url.startsWith('/ray-test')) {
-        target = 'https://anycast.cloudflare.tunnel.cloud-private.eu.org/'
+    if (req.url.startsWith('/ray-test/')) {
+        target = 'https://anycast.cloudflare.tunnel.cloud-private.eu.org'
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
