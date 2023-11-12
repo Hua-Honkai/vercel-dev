619babaafbc8ce6f5f5eb79adffab464a377231e
Guo Yiyang <hua-honkai@qq.com>
2023-11-12 19:23:00 +0800

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
-const app = express();
-
-app.use('/ray-test', createProxyMiddleware({ 
-  target: 'https://anycast.cloudflare.tunnel.cloud-private.eu.org', // 目标服务器地址
-  changeOrigin: true, // 需要虚拟主机站点
-}));
-
-app.listen(3000);
+module.exports = (req, res) => {
+    let target = 'https://anycast.cloudflare.tunnel.cloud-private.eu.org'
+    if (req.url.startsWith('/ray-test')) {
+        target = 'https://anycast.cloudflare.tunnel.cloud-private.eu.org'
+    }
+    // 创建代理对象并转发请求
+    createProxyMiddleware({
+        target,
+        changeOrigin: true,
+        pathRewrite: {
+            // 通过路径重写，去除请求路径中的 `/api`
+            // 如果开启了,那么 /api/user/login 将被转发到 http://gmall-h5-api.atguigu.cn/user/login
+            //'^/api/': '/',
+        },
+    })(req, res)
+}
