f61002ddea4eac0e8f8696b7a36039d74fc472bb
Suyunmeng <hua-honkai@qq.com>
2024-02-02 20:10:28 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  let targetUrl = req.url.slice(1); // remove '/' from the URL
-  targetUrl = 'https://' + targetUrl;
-
-  const options = {
-    target: targetUrl,
+  let target = req.url.replace('/cloud/', '');
+  createProxyMiddleware({
+    target,
     changeOrigin: true,
-    pathRewrite: (path, req) => path.split('/').slice(1).join('/'),
     onProxyRes: function (proxyRes, req, res) {
       proxyRes.headers['access-control-allow-origin'] = '*';
-    }
-  };
-
-  const proxy = createProxyMiddleware(options);
-  proxy(req, res, () => {});
+    },
+  })(req, res);
 };
